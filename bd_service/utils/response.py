d314fdebb8fb45ee2881e9ad0eaf8a2556f4587c
cornelita <cornelitalugitasantoso@gmail.com>
2022-05-21 22:59:33 +0700

    feat: response utils

---
+++ bd_service/utils/response.py
@@ -0,0 +1,20 @@
+from rest_framework import status
+from rest_framework.response import Response
+
+
+def success_post_response(bulkDownloadId):
+    return Response(
+        data={
+            'bulkDownloadId': bulkDownloadId,
+        },
+        status=status.HTTP_200_OK,
+    )
+
+
+def error_response(message, response_status=status.HTTP_500_INTERNAL_SERVER_ERROR):
+    return Response(
+        data={
+            'detail': message,
+        },
+        status=response_status,
+    )
