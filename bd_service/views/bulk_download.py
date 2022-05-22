9ca48e44061a71f0f262ba92f160f9e3643698e6
cornelita <cornelitalugitasantoso@gmail.com>
2022-05-23 00:49:48 +0700

    fix: implement async bulk download

--- bd_service/views/bulk_download.py
+++ bd_service/views/bulk_download.py
@@ -2,24 +2,25 @@ import logging
 import random
 from datetime import datetime
 
-import grpc
-import requests
+import redis
 from django.http import HttpResponse
 from rest_framework import status
-from rest_framework.response import Response
 from rest_framework.views import APIView
+from rq import Queue
 
 from bd_service.utils.response import error_response
 from bd_service.utils.response import success_post_response
-from download_pb2 import VideoRequest
-from download_pb2_grpc import DownloadServiceStub
+from bd_service.views.data import delete_progress_and_download_data
+from bd_service.views.data import get_download_data_by_key
+from bd_service.views.worker import download_videos
+
+r = redis.Redis(host='redis', port=6379)
+q = Queue(connection=r)
 
 logger = logging.getLogger(__name__)
 
 
 class BulkDownloadView(APIView):
-    download_data = {}
-
     def get(self, request):
         logger.info(f'[BulkDownload] GET request received: {datetime.now()}')
 
@@ -27,11 +28,12 @@ class BulkDownloadView(APIView):
         if bulk_download_id is None:
             return error_response('Parameter "bulkDownloadId" is required', status.HTTP_400_BAD_REQUEST)
 
-        data = self.download_data.get(bulk_download_id)
+        data = get_download_data_by_key(bulk_download_id)
         if data is None:
-            return error_response(f'Data with id {bulk_download_id} cannot be found', status.HTTP_404_NOT_FOUND)
+            return error_response(f'Data with id {bulk_download_id} cannot be found or still in progress', status.HTTP_404_NOT_FOUND)
 
         logger.info(f'[BulkDownload] GET request success: {datetime.now()}')
+        delete_progress_and_download_data(bulk_download_id)
         return HttpResponse(data)
 
     def post(self, request):
@@ -42,33 +44,7 @@ class BulkDownloadView(APIView):
             return error_response('Parameter "videoIds" is required', status.HTTP_400_BAD_REQUEST)
 
         key = str(random.randint(0, 1000)) + str(datetime.now().timestamp())
-        self.download_videos(key, video_ids)
+        q.enqueue(download_videos, args=(key, video_ids))
 
         logger.info(f'[BulkDownload] POST request success: {datetime.now()}')
         return success_post_response(key)
-
-    def download_videos(self, key, video_ids):
-        logger.info(f'[BulkDownload] Starting download video: {key}')
-        paths = []
-        try:
-            with grpc.insecure_channel('localhost:50051') as channel:
-                stub = DownloadServiceStub(channel)
-                for video_id in video_ids:
-                    response = stub.DownloadVideo(
-                        VideoRequest(videoId=video_id))
-                    paths.append(response.filePath)
-                    logger.info(
-                        f'[BulkDownload] Download video path received: {response.filePath}')
-        except grpc.RpcError as e:
-            logger.error(f'[BulkDownload] gRPC error raised: {e.details()}')
-            return Response(data=f'Error raised: {e.details()}', status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-
-        self.zip_file(key, paths)
-
-    def zip_file(self, key, paths):
-        logger.info(f'[BulkDownload] Starting zipping video: {key}')
-        response = requests.post('http://localhost:8001/zipper/', json={
-            'videos': paths
-        })
-        logger.info(f'[BulkDownload] Zip file received: {datetime.now()}')
-        self.download_data[key] = response
