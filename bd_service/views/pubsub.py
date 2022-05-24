eaa004639fb46bdaa993029019f4b8bb508af063
cornelita <cornelitalugitasantoso@gmail.com>
2022-05-24 22:05:20 +0700

    feat: create listener - not working

---
+++ bd_service/views/pubsub.py
@@ -0,0 +1,39 @@
+from threading import Thread
+
+from asgiref.sync import async_to_sync
+from channels.layers import get_channel_layer
+from redis import Redis
+
+from bd_service.views.data import get_progress_data_by_key
+
+r = Redis(host='redis', port=6379)
+
+
+class Listener(Thread):
+    def __init__(self, r, channels):
+        Thread.__init__(self)
+        self.redis = r
+        self.pubsub = self.redis.pubsub()
+        self.pubsub.subscribe(channels)
+
+    def work(self, item):
+        print(item['channel'], ":", item['data'])
+        self.notify_update_progress(item['channel'])
+
+    def run(self):
+        print("running")
+        for item in self.pubsub.listen():
+            self.work(item)
+
+    def notify_update_progress(self, bulk_download_id):
+        print('notify_update_progress')
+        print(bulk_download_id)
+        channel_layer = get_channel_layer()
+        progress_data = get_progress_data_by_key(bulk_download_id)
+        async_to_sync(channel_layer.group_send)(
+            bulk_download_id,
+            {
+                'type': 'notify_update_progress',
+                'message': progress_data
+            }
+        )
