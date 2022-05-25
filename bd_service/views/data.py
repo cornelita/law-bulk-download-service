cb55832a143b140434be451d281a329ea188a395
cornelita <cornelitalugitasantoso@gmail.com>
2022-05-25 20:44:54 +0700

    fix: change key format

--- bd_service/views/data.py
+++ bd_service/views/data.py
@@ -5,30 +5,30 @@ r = Redis(host='redis', port=6379)
 
 
 def set_progress_data(key, value):
-    r.set(f'{key}:P', value, ex=3600)
+    r.set(f'progress:{key}', value, ex=3600)
 
 
 def get_progress_data_by_key(key):
-    return r.get(f'{key}:P')
+    return r.get(f'progress:{key}')
 
 
 def set_download_data(key, value):
-    r.set(f'{key}:D', value, ex=3600)
+    r.set(f'download:{key}', value, ex=3600)
 
 
 def get_download_data_by_key(key):
-    return r.get(f'{key}:D')
+    return r.get(f'download:{key}')
 
 
 def get_multiple_download_data_by_key(keys):
-    keys_formatted = [f'{key}:D' for key in keys]
+    keys_formatted = [f'download:{key}' for key in keys]
     return r.mget(keys_formatted)
 
 
 def get_multiple_progress_data_by_key(keys):
-    keys_formatted = [f'{key}:P' for key in keys]
+    keys_formatted = [f'progress:{key}' for key in keys]
     return r.mget(keys_formatted)
 
 
 def delete_progress_and_download_data(key):
-    r.delete(f'{key}:D', f'{key}:P')
+    r.delete(f'download:{key}', f'progress:{key}')
