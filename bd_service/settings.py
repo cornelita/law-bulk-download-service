214e0cc97db37e10634257f2afb4dede6c74f33a
cornelita <cornelitalugitasantoso@gmail.com>
2022-05-26 14:24:56 +0700

    fix: change secret key

--- bd_service/settings.py
+++ bd_service/settings.py
@@ -25,7 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-__79b%r*!yi=w(72)o0z1n!4332(eiic&i^g!44956k9jes+gi'
+SECRET_KEY = os.getenv(
+    'SECRET_KEY', 'django-insecure-__79b%r*!yi=w(72)o0z1n!4332(eiic&i^g!44956k9jes+gi')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False if os.getenv('DEBUG', True) == 'False' else True
